d500c3d34d2e35502bac281c195bbd870cc7387a
munifhasan <mdmunifhasan862@gmail.com>
2023-04-02 14:32:56 +0600

    flexbox test

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.box1}>
+        <Text>1</Text>
+      </View>
+      <View style={styles.box2}>
+        <Text>2</Text>
+      </View>
+      <View style={styles.box3}>
+        <Text>3</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  appContainer: {
+    padding: 50,
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    gap: 20,
   },
-});
+  box1: {
+    backgroundColor: 'red',
+    width: 50,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box2: {
+    backgroundColor: 'green',
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box3: {
+    backgroundColor: 'blue',
+    width: 50,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '80%',
+    marginRight: 8,
+    paddingLeft: 8,
+  },
+})
