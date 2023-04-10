059ff3b51a65d0e76ae1023798de24e351c29799
munifhasan <mdmunifhasan862@gmail.com>
2023-04-10 20:43:50 +0600

    03. creating screen

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput } from 'react-native'
+import { View } from 'react-native'
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  )
+}
+
+export default StartGameScreen
