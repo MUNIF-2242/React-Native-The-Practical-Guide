2150e538aaa0031e5ff8eeea95f365c7b56325af
MUNIF <munif@shellbeehaken.com>
2023-04-11 14:39:20 +0600

    006 Styling the Number Input Element

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,7 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} textAlign='center' />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -26,5 +26,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 }, //shadow style for iOS
     shadowRadius: 6, //shadow style for iOS
     shadowOpacity: 0.25, //shadow style for iOS
+  },
+
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
   },
 });
