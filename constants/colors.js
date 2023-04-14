f71793b9a20fdf838b1a6a8b8fb17c827fd0dabc
munifhasan <mdmunifhasan862@gmail.com>
2023-04-14 22:21:09 +0600

    020 Creating, Using & Displaying Random Numbers

--- constants/colors.js
+++ constants/colors.js
@@ -2,6 +2,7 @@ const Colors = {
   primary500: '#720063',
   primary600: '#640233',
   accent500: '#ddb52f',
+  whiteColor: '#ffffff',
 }
 
 export default Colors
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,12 +1,27 @@
 import { Text, View, StyleSheet } from 'react-native'
-import Title from '../components/Title'
+import Title from '../components/ui/Title'
 import Colors from '../constants/colors'
+import { useState } from 'react'
+import NumberContainer from '../components/game/NumberContainer'
 
-function GameScreen() {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude)
+  } else {
+    return rndNum
+  }
+}
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber)
+  const [currentGuess, setCurrentGuess] = useState(initialGuess)
+
   return (
     <View style={styles.screen}>
-      <Title>Opponent's Guess</Title>
-      {/* GUESS */}
+      <Title style={styles.title}>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
         {/* + - */}
@@ -22,14 +37,5 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: Colors.accent500,
-    textAlign: 'center',
-    borderWidth: 2,
-    borderColor: Colors.accent500,
-    padding: 12,
   },
 })
