c303333f072448b1979bc6814ba1520b8ef97df3
MUNIF <munif@shellbeehaken.com>
2023-04-18 15:13:55 +0600

    005 Getting Started with the React Navigation Package

--- App.js
+++ App.js
@@ -2,13 +2,21 @@ import { SafeAreaView, StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { StatusBar } from "expo-status-bar";
 
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <>
-      <StatusBar style='light' />
-      <SafeAreaView style={styles.container}>
-        <CategoriesScreen />
-      </SafeAreaView>
+      <StatusBar style='dark' />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Meals Categories' component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
