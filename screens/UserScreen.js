632b13024f1496ea26f35997cf0b090f702860dc
MUNIF <munif@shellbeehaken.com>
2023-05-04 12:46:09 +0600

    019 Adding Drawer Navigation & Creating a Drawer

---
+++ screens/UserScreen.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function UserScreen() {
+  return <Text>UserScreen</Text>;
+}
+
+export default UserScreen;
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function WelcomeScreen() {
+  return <Text>WelcomeScreen</Text>;
+}
+
+export default WelcomeScreen;
