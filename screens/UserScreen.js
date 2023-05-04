05647a310e2593033f4337bfcf009eea90977dbd
MUNIF <munif@shellbeehaken.com>
2023-05-04 14:16:07 +0600

    020 Configuring the Drawer Navigator & The Drawer

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,15 +1,24 @@
-import { Button, Text } from "react-native";
+import { Button, Text, View, StyleSheet } from "react-native";
 
 function UserScreen({ navigation }) {
   function openDrawerHandler() {
     navigation.toggleDrawer();
   }
   return (
-    <>
-      <Text>User Screen</Text>
+    <View style={styles.container}>
+      <Text>User Screen..</Text>
       <Button onPress={openDrawerHandler} title="Open Drawer" />
-    </>
+    </View>
   );
 }
 
 export default UserScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
