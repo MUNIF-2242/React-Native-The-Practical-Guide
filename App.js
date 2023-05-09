6688a386b3a238333be2dccc688d7add154fca82
MUNIF <munif@shellbeehaken.com>
2023-05-09 14:23:29 +0600

    002 The Starting Setup

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import AllExpenses from "./screens/AllExpenses";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Expense Tracker App</Text>
+      <AllExpenses />
       <StatusBar style="auto" />
     </View>
   );
