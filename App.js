3712663c1fc26a303f803add7161f5480075810b
munifhasan <mdmunifhasan862@gmail.com>
2023-05-09 22:27:32 +0600

    positioning a view in various corner

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.innerContainer}></View>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //TOP LEFT
+
+    //CENTER LEFT
+    // justifyContent: 'center',
+
+    //BOTTOM LEFT
+    // justifyContent: 'flex-end',
+
+    //TOP RIGHT
+    // alignItems: 'flex-end',
+
+    //CENTER RIGHT
+    // alignItems: 'flex-end',
+    // justifyContent: 'center',
+
+    //BOTTOM RIGHT
+    // alignItems: 'flex-end',
+    // justifyContent: 'flex-end',
   },
-});
+  innerContainer: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'red',
+  },
+})
